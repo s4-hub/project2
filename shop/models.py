934c3c0a3a5528aef68ac812b70de46ddd31dc76
s4-hub <syafii.newbie@gmail.com>
2019-11-21 06:44:41 -0500

    Update today

---
+++ shop/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from django.urls import reverse
+from django.template.defaultfilters import slugify
+import uuid
+import os
+import datetime
+
+# Create your models here.
+
+
+def get_file_path(instance, filename):
+    today = datetime.datetime.now()
+    year = today.strftime('%Y')
+    month = today.strftime('%m')
+    day = today.strftime('%d')
+    output = 'products' + '/' + year + '/' + month + '/' + day + '/'
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join(output, filename)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,
+                            db_index=True)
+    slug = models.SlugField(max_length=200,
+                            unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
+
+
+SIZE_CHOICE = [
+    (1, 'SMALL'),
+    (2, 'MEDIUM'),
+    (3, 'LARGE'),
+    (4, 'EXTRA LARGE'),
+]
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to=get_file_path,
+                              blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # size = models.IntegerField(choices=SIZE_CHOICE, blank=True, null=True)
+    # berat = models.DecimalField(max_digits=5, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_single',
+                       args=[self.id, self.slug])
